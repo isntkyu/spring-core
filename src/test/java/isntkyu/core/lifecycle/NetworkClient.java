6dfb9fe807fc851e175c3bde725243de07b1f708
isntkyu <jkjkjk5710@gmail.com>
2022-01-31 18:42:22 +0900

    InitializingBean DisposableBean

---
+++ src/test/java/isntkyu/core/lifecycle/NetworkClient.java
@@ -0,0 +1,41 @@
+package isntkyu.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient  implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("url = " + url);
+
+    }
+    
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public void connect() {
+        System.out.println("connect= " + url);
+    }
+    
+    public void call(String message) {
+        System.out.println("url = " + url + " message = " + message);
+    }
+
+    public void disconnect(){
+        System.out.println("close = " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+}
